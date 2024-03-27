67ef35f708ed5f473cb3d729052d49f4122e811a
tanaywagh101 <tanaywagh01@gmail.com>
2024-03-27 11:46:41 +0530

    Microfrontenc integration done

--- servicetwo/vite.config.ts
+++ servicetwo/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "servicetwo",
+      filename: "remoteEntryTwo.js",
+      exposes: {
+        "./DemoComp": "./src/components/DemoComp.tsx",
+      },
+      shared: ["react"],
+    }),
+  ],
+});
