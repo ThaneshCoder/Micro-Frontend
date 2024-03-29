f7df70dee9125f9529d70fd82a77a7b79096addb
sneha <snehaannappaswamy92@gmail.com>
2024-03-29 14:35:12 +0530

    redux

--- mainapp/src/components/LandingPage.tsx
+++ mainapp/src/components/LandingPage.tsx
@@ -1,40 +1,33 @@
 import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { useNavigate } from "react-router-dom";
 import CardComp from "serviceone/CardComp";
+import { useSelector } from "react-redux";
+import { RootStore } from "../redux/Store";
 
-interface Product {
-    image: string;
-    name: string;
-    model: string;
-    price: number;
-    discount: number;
-    category: string;
-    total: number;
-  }
+// interface Product {
+//     image: string;
+//     name: string;
+//     model: string;
+//     price: number;
+//     discount: number;
+//     category: string;
+//     total: number;
+//   }
 
-interface LandingPageProps {
-  product: Product[]; // Adjust the type according to your actual data structure
-}
+// interface LandingPageProps {
+//   product: Product[]; // Adjust the type according to your actual data structure
+// }
 
-const LandingPage: React.FC<LandingPageProps> = ({ product }) => {
+const LandingPage= () => {
+  let selector = useSelector((state: RootStore) => state.product.allProducts);
   const navigate = useNavigate();
 
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
+        <AppBar position="fixed">
           <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Welcome
             </Typography>
@@ -46,7 +39,7 @@ const LandingPage: React.FC<LandingPageProps> = ({ product }) => {
           </Toolbar>
         </AppBar>
       </Box>
-      <CardComp productCard={product} />
+      <CardComp product={selector} />
     </div>
   );
 };
--- servicetwo/src/App.tsx
+++ servicetwo/src/App.tsx
@@ -1,10 +1,9 @@
-import Summary from "./components/Summary";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <Summary />
+      
     </div>
   );
 }
